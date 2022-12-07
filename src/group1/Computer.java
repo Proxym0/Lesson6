ad06663e90d9d10ca9d1456fceca21c7397ed5d1
Proxym0 <vladi.shapovalov@gmail.com>
2022-12-07 23:45:02 +0300

    save2

--- src/group1/Computer.java
+++ src/group1/Computer.java
@@ -10,12 +10,14 @@ public class Computer {
         this.price = price;
         this.model = model;
     }
-    public  Computer(int price, String model, RAM ram, HDD hdd) {
+
+    public Computer(int price, String model, RAM ram, HDD hdd) {
         this.price = price;
         this.model = model;
         this.ram = ram;
         this.hdd = hdd;
     }
+
     @Override
     public String toString() {
         return "Computer{" +
@@ -26,11 +28,48 @@ public class Computer {
                 '}';
     }
 
-    public static void main(String[] args) {
-        Computer computer1=new Computer(10,"apple");
-        Computer computer2=new Computer(15,"dell");
-        System.out.println(computer1);
-        System.out.println(computer2);
 
+    class RAM {
+        String name;
+        int volume;
+
+        RAM(String name, int volume) {
+            this.name = name;
+            this.volume = volume;
+        }
+
+        public RAM() {
+        }
+
+        @Override
+        public String toString() {
+            return "RAM{" +
+                    "name='" + name + '\'' +
+                    ", volume=" + volume +
+                    '}';
+        }
     }
-}
+        public class HDD {
+            String name;
+            int volume;
+            String externaltype;
+            String internaltype2;
+
+            public HDD(String name, int volume, String externaltype, String internaltype2) {
+                this.name = name;
+                this.volume = volume;
+                this.externaltype = externaltype;
+                this.internaltype2 = internaltype2;
+            }
+
+            public HDD() {
+            }
+        }
+            public static void main(String[] args) {
+                Computer computer1 = new Computer(10, "apple");
+                Computer computer2 = new Computer(15, "dell");
+                System.out.println(computer1);
+                System.out.println(computer2);
+            }
+        }
+
