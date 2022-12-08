40509bf5f58cbb24f93ccb4ef7c33baece19a008
Proxym0 <vladi.shapovalov@gmail.com>
2022-12-08 20:36:59 +0300

    save3

--- src/group1/Computer.java
+++ src/group1/Computer.java
@@ -6,9 +6,11 @@ public class Computer {
     RAM ram;
     HDD hdd;
 
-    public Computer(int price, String model) {
-        this.price = price;
-        this.model = model;
+    public Computer() {
+        price = 10;
+        model ="apple";
+        this.ram = new RAM();
+        this.hdd = new HDD();
     }
 
     public Computer(int price, String model, RAM ram, HDD hdd) {
@@ -16,8 +18,8 @@ public class Computer {
         this.model = model;
         this.ram = ram;
         this.hdd = hdd;
-    }
 
+    }
     @Override
     public String toString() {
         return "Computer{" +
@@ -27,20 +29,32 @@ public class Computer {
                 ", hdd=" + hdd +
                 '}';
     }
+    public static void main(String[] args) {
+        Computer.RAM ram = new RAM("app",256);
+        Computer.HDD hdd=new HDD("ddr",256,"12");
+        Computer computer1 = new Computer();
+        Computer computer2 = new Computer(20,"DELL",ram,hdd);
 
+//        computer2.internaltype2;
+        System.out.println(computer1);
+        System.out.println(computer2);}
 
-    class RAM {
+    static class RAM {
         String name;
         int volume;
 
-        RAM(String name, int volume) {
+        public RAM(String name, int volume) {
             this.name = name;
             this.volume = volume;
         }
 
         public RAM() {
+            name = "ddr2";
+            volume =512;
         }
+        void ram1(){
 
+        }
         @Override
         public String toString() {
             return "RAM{" +
@@ -49,27 +63,35 @@ public class Computer {
                     '}';
         }
     }
-        public class HDD {
+        public static class HDD {
             String name;
             int volume;
             String externaltype;
-            String internaltype2;
 
-            public HDD(String name, int volume, String externaltype, String internaltype2) {
+            public HDD(String name, int volume, String externaltype) {
                 this.name = name;
                 this.volume = volume;
                 this.externaltype = externaltype;
-                this.internaltype2 = internaltype2;
+
             }
 
             public HDD() {
+                name="ddr";
+                volume=256;
+                externaltype="12";
+
+
+            }
+
+            @Override
+            public String toString() {
+                return "HDD{" +
+                        "name='" + name + '\'' +
+                        ", volume=" + volume +
+                        ", externaltype='" + externaltype + '\'' +
+                        '}';
             }
         }
-            public static void main(String[] args) {
-                Computer computer1 = new Computer(10, "apple");
-                Computer computer2 = new Computer(15, "dell");
-                System.out.println(computer1);
-                System.out.println(computer2);
-            }
-        }
+}
+
 
