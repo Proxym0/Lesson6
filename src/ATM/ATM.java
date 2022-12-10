d03fed0b78643757e6c49d556feb4e05db9da5a4
Proxym0 <vladi.shapovalov@gmail.com>
2022-12-10 18:38:23 +0300

    save5

--- src/ATM/ATM.java
+++ src/ATM/ATM.java
@@ -4,57 +4,59 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class ATM {
-    int bill20;
-    int bill50;
-    int bill100;
+    double bill20;
+    double bill50;
+    double bill100;
     Scanner scanner = new Scanner(System.in);
-    static int sum;
-    int money= scanner.nextInt();;
-
-    public ATM(int bill20, int bill50, int bill100, int sum) {
+    static double sum;
+    double money = scanner.nextDouble();
+    public ATM(double bill20, double bill50, double bill100, double sum) {
         this.bill20 = bill20;
         this.bill50 = bill50;
         this.bill100 = bill100;
-        this.sum = 1000;
+        this.sum = 1100;
     }
     public ATM() {
     }
     public static void main(String[] args) {
-        ATM cash = new ATM(100, 10, 8, sum);
+        ATM cash = new ATM(100.0, 3.0, 8.0, sum);
         cash.noMoney();
         cash.remainder();
         System.out.println(cash);
-//    }
-//    Object summa() {
-//        String what = scanner.nextLine();
-//        if (Objects.equals(what, "Снять наличные")) {
-//            Object remainder = null;
-//            return remainder;
-//        }
-//        return null;
     }
-    int noMoney(){
-        if (sum <money) {
-                System.out.println("недостаточно денег на счету");
-            }return money= scanner.nextInt();
+    double noMoney() {
+        if (sum < money) {
+            System.out.println("недостаточно денег на счету");
+        }
+        return money = scanner.nextDouble();
     }
-
     void remainder() {
         sum -= money;
-        if (bill100-money / 100 >= 0&&money % 100 == 0) {
+        if ((bill50 - (money / 50)) >= 0 && money % 50 == 0) {
+            bill50 = bill50 - money / 50;
+            System.out.println("На счету осталось :" + sum + " " + bill50 + " купюр наминалом в 50 рублей "
+                    + bill100 + " купюр наминалом в 100 рублей " + bill20 + " купюр наминалом в 20 рублей ");
+        } else if (bill100 - money / 100 >= 0 && money % 100 == 0) {
             bill100 = bill100 - (money / 100);
-            System.out.println("Вы сняли :" + money / 100+" купюр номиналом 100");
-        } else if (bill100-money / 100 < 0&&money % 50==0) {
-            bill50 =bill50+ 2*(bill100 - (money / 100));
-            bill100=bill100-bill100;
-
-            System.out.println("На счету осталось :"+sum+" " + bill50+" купюр наминалом в 50 рублей "
-                    +bill100+" купюр наминалом в 100 рублей"+bill20+" купюр наминалом в 20 рублей " );
-        } else if (money % 20 == 0) {
-            bill20 = bill50 - (money / 20);
-            System.out.println("На счету осталось :" + bill20);
+            System.out.println("На счету осталось :" + sum + " " + bill50 + " купюр наминалом в 50 рублей "
+                    + bill100 + " купюр наминалом в 100 рублей" + bill20 + " купюр наминалом в 20 рублей ");
+        } else if (bill100 - money / 100 < 0 && money % 50 == 0 && bill50 / 2 + bill100 - money / 100 >= 0) {
+            bill50 = bill50 * 2 + bill100 - (money / 100);
+            bill100 = 0.0;
+            System.out.println("На счету осталось :" + sum + " " + bill50 + " купюр наминалом в 50 рублей "
+                    + bill100 + " купюр наминалом в 100 рублей" + bill20 + " купюр наминалом в 20 рублей ");
+        } else if (bill100 - money / 100 < 0 && bill20 * 5 + bill50 * 2 + bill100 - money / 100 >= 0 && money % 20 == 0) {
+            bill20 = bill20 + 2 * (bill50 + 2 * (bill100 - (money / 100)));
+            bill100 = 0.0;
+            bill50 = 0.0;
+            System.out.println("На счету осталось :" + sum + " " + bill50 + " купюр наминалом в 50 рублей "
+                    + bill100 + " купюр наминалом в 100 рублей " + bill20 + " купюр наминалом в 20 рублей ");
+        } else if (money % 20 == 0 && bill20 - money / 20 >= 0) {
+            bill20 = bill20 - (money / 20);
+            System.out.println("На счету осталось :" + sum + " " + bill50 + " купюр наминалом в 50 рублей "
+                    + bill100 + " купюр наминалом в 100 рублей " + bill20 + " купюр наминалом в 20 рублей ");
         }
-            }
+    }
         @Override
         public String toString () {
             return "ATM{" +
